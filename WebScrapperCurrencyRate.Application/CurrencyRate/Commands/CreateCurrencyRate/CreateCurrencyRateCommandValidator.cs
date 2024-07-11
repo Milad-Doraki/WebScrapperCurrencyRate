eb6f62474aee046e8b90f742d67e3b63b7d46a06
Milad Doraki <milad.doraki@gmail.com>
2024-07-11 21:36:36 +0330

    space

--- WebScrapperCurrencyRate.Application/CurrencyRate/Commands/CreateCurrencyRate/CreateCurrencyRateCommandValidator.cs
+++ WebScrapperCurrencyRate.Application/CurrencyRate/Commands/CreateCurrencyRate/CreateCurrencyRateCommandValidator.cs
@@ -10,6 +10,7 @@ namespace WebScrapperCurrencyRate.Application.CurrencyRates.Commands.CreateCurre
             RuleFor(currencyRate => currencyRate.Currency).NotEmpty();
             RuleFor(currencyRate => currencyRate.Buy).GreaterThanOrEqualTo(0);
             RuleFor(currencyRate => currencyRate.Sell).GreaterThanOrEqualTo(0);
+
         }
     }
 }
